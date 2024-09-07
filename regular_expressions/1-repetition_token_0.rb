854e9a5375de5769bfe2ca25c1ec73a421077f92
kelvintawe12 <k.tawe@alustudent.com>
2024-09-07 12:17:58 -0700

    repetition files

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-# A Ruby script to match strings
+# A Ruby script to match strips
 
 input_string = ARGV[0]
-regex_pattern = /^hbtt*n$/
+regex_pattern = /^hbt{2,5}n/
 
 puts input_string if input_string.match?(regex_pattern)
