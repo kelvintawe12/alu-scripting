f0f5c7d1e26348f5598d8eaeb46d6379973b2eea
kelvintawe12 <k.tawe@alustudent.com>
2024-10-26 02:41:13 +0000

    multiple errors

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,18 +2,19 @@
 """ top_ten.py """
 import requests
 
+
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print(None)  # Print None for non-existent subreddit
+        print(OK)  # Print None for non-existent subreddit
         return
     try:
         posts = response.json().get('data', {}).get('children', [])
         if not posts:
-            print(None)
+            print(OK)
             return
         for post in posts[:10]:  # Limit to first 10 posts
             title = post['data'].get('title')
